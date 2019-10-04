499ceb5735c8cf4348eae9e13a515376d1f9beaa
Erick <erickhenrique321@gmail.com>
2019-10-04 11:00:48 -0300

    Home icon size; mock login

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
 import { setUserToken } from '../services/async-storage';
 
 export const login = async (username, password) => {
-  const token = '6555149815';
-  setUserToken(token);
+  const userData = {
+    token: '6555149815',
+    name: 'Erick Henrique dos Santos',
+    email: 'erickhenrique321@gmail.com',
+    password: '12345',
+  };
+
+  setUserToken(JSON.stringify(userData.token));
 };

8e68e7e3e4dd8562fec6101d398e471e91800943
Erick <erickhenrique321@gmail.com>
2019-10-20 20:22:25 -0300

    image change

--- src/pages/Configuration/index.js
+++ src/pages/Configuration/index.js
@@ -5,9 +5,17 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faUserCog } from '@fortawesome/free-solid-svg-icons';
+import ImagePicker from 'react-native-image-picker';
+
+const options = {
+  title: 'Selecionar foto do exame',
+  takePhotoButtonTitle: 'Tirar foto',
+  chooseFromLibraryButtonTitle: 'Escolher foto da galeria',
+};
 
 export default class Configuration extends React.Component {
   state = {
@@ -30,9 +38,35 @@ export default class Configuration extends React.Component {
     this.props.navigation.navigate('Login');
   };
 
+  choosePhoto = () => {
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('Image Picker Error: ', response.error);
+      } else {
+        let source = { uri: response.uri };
+
+        this.setState({
+          avatarSource: source,
+          pic: response.data,
+        });
+      }
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        <Image
+          source={this.state.avatarSource}
+          style={{ width: 92, height: 94, borderRadius: 100 }}
+        />
+        <TouchableOpacity onPress={this.choosePhoto}>
+          <Text>Alterar</Text>
+        </TouchableOpacity>
         <TouchableOpacity onPress={this.logout}>
           <Text>Logout</Text>
         </TouchableOpacity>

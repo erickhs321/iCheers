134cfddea4a7ebdd66a63f8c989eb432d28f598a
Erick <erickhenrique321@gmail.com>
2019-10-20 20:07:06 -0300

    config page started

--- src/pages/Configuration/index.js
+++ src/pages/Configuration/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faUserCog } from '@fortawesome/free-solid-svg-icons';
 
 export default class Configuration extends React.Component {
+  state = {
+    email: 'erickhenrique321@gmail.com',
+    password: '123456',
+    height: '',
+    weight: '',
+    dateOfBirth: '',
+    bloodType: 'O+',
+  };
+
   static navigationOptions = {
     tabBarLabel: 'Configuração',
     tabBarIcon: ({ tintColor }) => (
@@ -17,11 +32,71 @@ export default class Configuration extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.container}>
         <TouchableOpacity onPress={this.logout}>
           <Text>Logout</Text>
         </TouchableOpacity>
+        <Text style={styles.title}>Email</Text>
+        <TextInput
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+          style={styles.input}
+        />
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+          value={this.state.password}
+          onChangeText={password => this.setState({ password })}
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <Text style={styles.title}>Altura</Text>
+        <TextInput
+          value={this.state.height}
+          onChangeText={height => this.setState({ height })}
+          style={styles.input}
+        />
+        <Text style={styles.title}>Peso(kg)</Text>
+        <TextInput
+          value={this.state.weight}
+          onChangeText={weight => this.setState({ weight })}
+          style={styles.input}
+        />
+        <Text style={styles.title}>Data de Nascimento</Text>
+        <TextInput
+          value={this.state.dateOfBirth}
+          onChangeText={dateOfBirth => this.setState({ dateOfBirth })}
+          style={styles.input}
+        />
+        <Text style={styles.title}>Tipo Sanguíneo</Text>
+        <TextInput
+          value={this.state.bloodType}
+          onChangeText={bloodType => this.setState({ bloodType })}
+          style={styles.input}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 40,
+    width: '100%',
+  },
+  title: {
+    alignSelf: 'flex-start',
+    marginBottom: -15,
+    color: 'rgba(51, 51, 51, 0.7)',
+    fontSize: 12,
+  },
+  input: {
+    width: '100%',
+    borderBottomColor: '#363740',
+    borderBottomWidth: 1.1,
+    paddingLeft: 0,
+    fontSize: 14,
+    paddingBottom: 0,
+    marginBottom: 20,
+  },
+});

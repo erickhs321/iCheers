45e6233badbcbc6971a26a5d28fddd0fc7ae22f8
rafaalvesf <37000965+rafaalvesf@users.noreply.github.com>
2019-10-22 21:57:28 -0300

    Ultima Alteração QR CODE
    Voltando para ultima alteração QR CODE

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,179 +1,109 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
-
-mobiscroll.settings = {
-    lang: 'pt-BR',
-    theme: 'ios'
-};
-
-class ListItem extends React.Component {
-    render() {
-        return <li data-icon={this.props.item.icon}>{this.props.item.text}</li>;
-    }
+export default class Quiz extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.box}>
+          <Text style={styles.text1}> Pergunta </Text>
+          <Text style={styles.text2}>
+            Texto da pergunda Desejada, qualquer Pergunta?
+          </Text>
+          <View style={styles.icons}>
+            <TouchableOpacity>
+              <Image
+                style={styles.iconsize}
+                source={require('../assets/qsad.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.iconsize}
+                source={require('../assets/qsad1.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.iconsize}
+                source={require('../assets/qnormal.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.iconsize}
+                source={require('../assets/qhappy.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                style={styles.iconsize}
+                source={require('../assets/qhappy1.png')}
+              />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.proxima}>
+            <Text style={styles.text3}> Pular </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            options: [{
-                id: 1,
-                text: 'Wifi',
-                icon: 'connection'
-            }, {
-                id: 2,
-                text: 'Location',
-                icon: 'location'
-            }, {
-                id: 3,
-                text: 'Sound',
-                icon: 'volume-medium'
-            }, {
-                id: 4,
-                text: 'Rotation',
-                icon: 'fa-rotate-left'
-            }, {
-                id: 5,
-                text: 'Bluetooth',
-                icon: 'ion-bluetooth'
-            }, {
-                id: 6,
-                text: 'Settings',
-                icon: 'cogs'
-            }, {
-                id: 7,
-                text: 'Reading',
-                icon: 'user4'
-            }, {
-                id: 8,
-                text: 'Data',
-                icon: 'download'
-            }, {
-                id: 9,
-                text: 'Eye comfort',
-                icon: 'eye'
-            }, {
-                id: 10,
-                text: 'Screenshot',
-                icon: 'mobile'
-            }, {
-                id: 11,
-                text: 'Airplane Mode',
-                icon: 'airplane'
-            }, {
-                id: 12,
-                text: 'Alarm',
-                icon: 'alarm2'
-            }, {
-                id: 13,
-                text: 'Messages',
-                icon: 'material-message'
-            }, {
-                id: 14,
-                text: 'Weather',
-                icon: 'meteo-weather4'
-            }, {
-                id: 15,
-                text: 'Camera',
-                icon: 'camera'
-            }, {
-                id: 16,
-                text: 'Edit',
-                icon: 'material-photo-size-select-large'
-            }]
-        };
-    }
-    
-    showPopup = () => {
-        this.refs.Popup.instance.show();
-    }
-    
-    showList = () => {
-        this.refs.list.instance.show();
-    }
-    
-    onSet = (event, inst) => {
-        this.setState({ checked: this.state.radio });
-    }
-    
-    myRadioChanged = (event, inst) => {
-        this.setState({ radio: event.target.value });
-    }
-    
-    onItemTap = (event, inst) => {
-        this.refs.scrollable.instance.hide();
-        mobiscroll.toast({
-            message: event.target.innerText + ' clicked'
-        });
-    }
-    
-    showScrollable = () => {
-        this.refs.scrollable.instance.show();
-    }
-    
-    render() {
-        return (
-            <div>
-                <mobiscroll.Form>
-                    <mobiscroll.FormGroup>
-                        <mobiscroll.FormGroupTitle>Without return value</mobiscroll.FormGroupTitle>
-                        <div className="mbsc-btn-group-block">
-                            <mobiscroll.Button onClick={this.showPopup}>Show Popup</mobiscroll.Button>
-                        </div>
-                    </mobiscroll.FormGroup>
-                    <mobiscroll.FormGroup>
-                        <mobiscroll.FormGroupTitle>With return value</mobiscroll.FormGroupTitle>
-                        <mobiscroll.Input onClick={this.showList} value={this.state.checked} placeholder="Please Select..." readOnly>Update</mobiscroll.Input>
-                    </mobiscroll.FormGroup>
-                    <mobiscroll.FormGroup>
-                        <mobiscroll.FormGroupTitle>With scrollable content</mobiscroll.FormGroupTitle>
-                        <div className="mbsc-btn-group-block">
-                            <mobiscroll.Button onClick={this.showScrollable}>Show Popup</mobiscroll.Button>
-                        </div>
-                    </mobiscroll.FormGroup>
-                </mobiscroll.Form>
-                <mobiscroll.Popup
-                    ref="Popup"
-                    display="center"
-                >
-                    <div className="mbsc-align-center mbsc-padding">
-                        <img src="https://img.mobiscroll.com/demos/f1.png" />
-                        <h3>Liza Taylor</h3>
-                        <p>liza.taylor@mobiscroll.com <br /> (202) 555-0127</p>
-                    </div>
-                </mobiscroll.Popup>
-                <mobiscroll.Popup
-                    ref="list"
-                    display="center"
-                    onSet={this.onSet}
-                >
-                    <mobiscroll.Form>
-                        <mobiscroll.FormGroup inset>
-                            <p>Some updates are available for you. <br /> When would you like to install them?</p>
-                        </mobiscroll.FormGroup>
-                        <mobiscroll.FormGroup inset>
-                            <mobiscroll.Radio name="update" value="Right now" checked={this.state.radio === 'Right now'} onChange={this.myRadioChanged}>Right now</mobiscroll.Radio>
-                            <mobiscroll.Radio name="update" value="Later on today" checked={this.state.radio === 'Later on today'} onChange={this.myRadioChanged}>Later on today</mobiscroll.Radio>
-                            <mobiscroll.Radio name="update" value="Remind me tomorrow" checked={this.state.radio === 'Remind me tomorrow'} onChange={this.myRadioChanged}>Remind me tomorrow</mobiscroll.Radio>
-                        </mobiscroll.FormGroup>
-                    </mobiscroll.Form>
-                </mobiscroll.Popup>
-                <mobiscroll.Popup
-                    ref="scrollable"
-                    display="center"
-                    scrollLock={false}
-                    cssClass="mbsc-no-padding md-content-scroll"
-                    buttons={[]}
-                >
-                    <mobiscroll.Listview 
-                        enhance={true}
-                        swipe={false}
-                        itemType={ListItem}
-                        data={this.state.options}
-                        onItemTap={this.onItemTap}
-                    />
-                </mobiscroll.Popup>
-            </div>
-        );
-    }    
-}
+const styles = StyleSheet.create({
+  box: {
+    width: 300,
+    height: 280,
+    backgroundColor: '#363740',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text1: {
+    padding: 10,
+    fontFamily: 'Muli',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 15,
+    lineHeight: 19,
+    textAlign: 'center',
+    letterSpacing: 0.3,
+    color: '#E64D57',
+  },
+  text2: {
+    padding: 15,
+    fontFamily: 'Muli',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 23,
+    textAlign: 'center',
+    letterSpacing: 0.3,
+    color: '#FFFFFF',
+    textShadowColor: 'rgba(0, 0, 0, 0.25)',
+    textShadowOffset: { width: 4, height: 4 },
+  },
+  text3: {
+    lineHeight: 23,
+    textAlign: 'center',
+    letterSpacing: 0.3,
+    color: '#ffff',
+  },
+  proxima: {
+    backgroundColor: '#E64D57',
+    width: 80,
+    height: 25,
+    borderRadius: 40,
+  },
+  iconsize: {
+    width: 40,
+    height: 40,
+    justifyContent: 'space-between',
+    marginLeft: 13,
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 35,
+  },
+});
--- src/pages/Exams/index.js
+++ src/pages/Exams/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import { Image, View, Text, Button } from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faFileMedicalAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFileMedicalAlt,
+  faFilter,
+  faPlus,
+  faEye,
+  Button,
+} from '@fortawesome/free-solid-svg-icons';
 import ImagePicker from 'react-native-image-picker';
 import Ocr from 'react-native-tesseract-ocr';
+import { DataTable } from 'react-native-paper';
 
 const options = {
   title: 'Selecionar foto do exame',
@@ -22,6 +36,20 @@ export default class Exams extends React.Component {
     this.state = {
       avatarSource: null,
       text: '',
+      exams: [
+        {
+          id: '1',
+          name: 'Exame toxicológico',
+          place: 'Laboratório São Marcos',
+          date: '25/05/2010',
+        },
+        {
+          id: '2',
+          name: 'Exame de sangue',
+          place: 'Laboratório São Marcos',
+          date: '25/05/2015',
+        },
+      ],
     };
   }
 
@@ -61,14 +89,153 @@ export default class Exams extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Image
-          source={this.state.avatarSource}
-          style={{ width: 200, height: 200, margin: 10, padding: 10 }}
-        />
-        <Button title="Selecione o exame" onPress={this.choosePhoto} />
-        <Text style>{this.state.text}</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <View style={styles.containerButton}>
+            <TouchableOpacity style={styles.filterButton}>
+              <FontAwesomeIcon
+                size={12}
+                icon={faFilter}
+                color={'#E64D57'}
+                style={styles.iconMargin}
+              />
+              <Text style={styles.filterButton}>Filtrar busca</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={this.choosePhoto}
+              style={styles.registerExamButton}>
+              <FontAwesomeIcon
+                size={12}
+                icon={faPlus}
+                color={'#01D300'}
+                style={styles.iconMargin}
+              />
+              <Text style={{ color: '#01D300', fontSize: 10 }}>
+                Cadastrar exame
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.vaccineCardButton}>
+              <FontAwesomeIcon
+                size={13}
+                icon={faEye}
+                color={'#252733'}
+                style={styles.iconMargin}
+              />
+              <Text style={styles.vaccineCardButton}>Ver cartão de vacina</Text>
+            </TouchableOpacity>
+          </View>
+          <DataTable style={styles.table}>
+            <DataTable.Header style={styles.borderBottomRow}>
+              <DataTable.Title>
+                <Text style={styles.titleStyle}>Nome</Text>
+              </DataTable.Title>
+              <DataTable.Title style={{ flex: 0.7 }}>
+                <Text style={styles.titleStyle}>Local</Text>
+              </DataTable.Title>
+              <DataTable.Title style={{ flex: 0.45 }}>
+                <Text style={styles.titleStyle}>Data</Text>
+              </DataTable.Title>
+            </DataTable.Header>
+
+            {this.state.exams.map(exam => {
+              return (
+                <DataTable.Row style={styles.borderBottomRow} key={exam.id}>
+                  <DataTable.Cell>
+                    <Text style={styles.tableTextSize}>{exam.name}</Text>
+                  </DataTable.Cell>
+                  <DataTable.Cell>
+                    <Text style={styles.tableTextSize}>{exam.place}</Text>
+                  </DataTable.Cell>
+
+                  <Text style={styles.tableTextSize}>
+                    {exam.date}
+                    {'    '}
+                  </Text>
+
+                  <Text style={styles.tableTextSize}>
+                    <TouchableOpacity style={styles.viewButton}>
+                      <FontAwesomeIcon size={14} icon={faEye} color={'#fff'} />
+                    </TouchableOpacity>
+                  </Text>
+                </DataTable.Row>
+              );
+            })}
+          </DataTable>
+
+          <Image
+            source={this.state.avatarSource}
+            style={{ width: 200, height: 200, margin: 10, padding: 10 }}
+          />
+          <Text style>{this.state.text}</Text>
+        </View>
+      </ScrollView>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 31,
+    alignItems: 'center',
+  },
+  table: {
+    width: '97%',
+  },
+  filterButton: {
+    flexDirection: 'row',
+    color: '#E64D57',
+    fontSize: 10,
+    fontWeight: '500',
+  },
+  registerExamButton: {
+    flexDirection: 'row',
+    color: '#01D300',
+    fontSize: 10,
+    marginLeft: 14,
+    marginRight: 14,
+  },
+  vaccineCardButton: {
+    flexDirection: 'row',
+    color: '#252733',
+    fontSize: 10,
+  },
+  borderBottomRow: {
+    borderBottomColor: '#DFE0EB',
+    borderBottomWidth: 1.5,
+  },
+  containerButton: {
+    flexDirection: 'row',
+    marginBottom: 19,
+  },
+  button: {
+    flexDirection: 'row',
+  },
+  titleStyle: {
+    color: '#9FA2B4',
+    fontSize: 11,
+  },
+  viewButton: {
+    backgroundColor: '#29CC97',
+    width: 32.3,
+    height: 15,
+    flex: 1,
+    alignSelf: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+  tableTextSize: {
+    fontSize: 10,
+    marginTop: 17,
+  },
+  iconMargin: {
+    marginRight: 2,
+  },
+  nameColumn: {
+    flex: 2,
+  },
+  placeColumn: {
+    flex: 2,
+  },
+  dateColumn: {
+    flex: 1,
+  },
+});

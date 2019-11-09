1933542482ae12a93548925e02259af14ae09157
rafaalvesf <37000965+rafaalvesf@users.noreply.github.com>
2019-11-09 12:29:17 -0300

    Revert "Revert "Adicionando o Gráfico""
    This reverts commit 794a5d1bbaa7d43ecb79e4db44541ac71ab3edf0.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import Quiz from '../../components/quiz';
+import { PieChart } from 'react-native-svg-charts';
+import { Circle, G, Line } from 'react-native-svg';
 
 export default class Home extends React.Component {
   static navigationOptions = {
@@ -11,6 +13,7 @@ export default class Home extends React.Component {
       <FontAwesomeIcon size={22} icon={faHome} color={tintColor} />
     ),
   };
+ 
 
   constructor(props) {
     super(props);
@@ -18,14 +21,47 @@ export default class Home extends React.Component {
       questions: ['Você está bem?', 'Como vai hoje?', 'Teste?'] || [],
     };
   }
-
+  
   render() {
+    const data = [ 50,19,53, 85, 91 ]
+
+    const randomColor = () => ('#' + (Math.random() * 0xFFFFFF << 0).toString(16) + '000000').slice(0, 7)
+
+    const pieData = data
+        .filter(value => value > 0)
+        .map((value, index) => ({
+            value,
+            svg: { fill: randomColor() },
+            key: `pie-${index}`,
+        }))
+
+    const Labels = ({ slices }) => {
+        return slices.map((slice, index) => {
+            const { labelCentroid, pieCentroid, data } = slice;
+        })
+    }
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home</Text>
-        <Quiz questions={this.state.questions} />
+      <View>
+        <View style={{marginTop: 180}}>
+          <Quiz questions={this.state.questions} />
+        </View>
+        <View style={{marginTop: 140, alignItems: 'center'}}>
+          <Text>Gráfico de Humor</Text>
+        </View>
+        <View>
+          <PieChart
+            style={ { height: 250 } }
+            data={ pieData }
+            innerRadius={ 40 }
+            outerRadius={ 105 }
+            labelRadius={ 80 }
+            >
+            <Labels/>
+          
+          </PieChart>
+        </View>      
       </View>
-    );
+    )
   }
 }
 
